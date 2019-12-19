c305bf9173124e75ee7a4911f9edabe84032d4f0
csamatov96 <csamatov96@gmail.com>
2019-12-19 14:54:42 -0600

    changes

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "ec2" {
   instance_type   = "t2.micro"
   associate_public_ip_address = "true"
   key_name = aws_key_pair.key_resource.key_name
-  security_groups = ["allow_ssh"]
+  security_groups = ["allow_ssh"] #attaching Sec Group by its name
   user_data = file("userdata_file")
 
   lifecycle{
