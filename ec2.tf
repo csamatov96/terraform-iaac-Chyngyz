53bab5845940cb6231c5bfeaf0e3c1ef6a4431c1
csamatov96 <csamatov96@gmail.com>
2019-12-19 14:53:18 -0600

    changes

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "ec2" {
   ami             = "ami-00068cd7555f543d5"
   instance_type   = "t2.micro"
   associate_public_ip_address = "true"
-  key_name = aws_key_pair.deployer.key_name
+  key_name = aws_key_pair.key_resource.key_name
   security_groups = ["allow_ssh"]
   user_data = file("userdata_file")
 
