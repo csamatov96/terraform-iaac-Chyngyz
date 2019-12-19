d207fe1ca271e5b1aae7f90211b80b8e41841a5f
csamatov96 <csamatov96@gmail.com>
2019-12-19 15:21:52 -0600

    changes

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "web" { #
   instance_type   = "t2.micro"
   associate_public_ip_address = "true"
   key_name = aws_key_pair.key_resource.key_name
-  security_groups = ["sec_group"]
+  security_groups = ["allow_ssh_http"] #refer only by its name not resource name
   user_data = file("userdata_file")
 
   lifecycle{
