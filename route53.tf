5bf4bcbecc8b2c5d787f085bbd4036dfe615fd96
csamatov96 <csamatov96@gmail.com>
2019-12-19 15:10:16 -0600

    changes

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "www" {
-  zone_id = "Z32OHGRMBVZ9LR"
-  name    = "www.acirrustech.com"
+  zone_id = "Z6XWWD56QDJWX"
+  name    = "www.csamatov.net"
   type    = "A"
   ttl     = "60"
   records = [aws_instance.web[0].public_ip,aws_instance.web[1].public_ip,aws_instance.web[2].public_ip]
