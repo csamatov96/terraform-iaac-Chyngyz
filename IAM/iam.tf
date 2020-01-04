6a5ed7198b92c0a534c49a900ab7ea2b7c1821aa
csamatov96 <csamatov96@gmail.com>
2020-01-04 11:30:56 -0600

    let's seee

--- IAM/iam.tf
+++ IAM/iam.tf
@@ -8,7 +8,7 @@ resource "aws_iam_group" "developers" {
     path = "/" 
 } 
 
-resource "aws_iam_group_membership" "team" {  #for terraform, specifying users to groups, terraform way of doing 
+/*resource "aws_iam_group_membership" "team" {  #for terraform, specifying users to groups, terraform way of doing 
   name = "developers-group-membership" 
   users = [ 
     aws_iam_user.tim.name 
@@ -33,4 +33,41 @@ resource "aws_iam_policy" "policy" {
 ] 
 } 
 EOF 
+} 
+*/
+
+resource "aws_iam_role" "test_role" { 
+
+name = "test_role" 
+
+assume_role_policy = <<EOF 
+
+{ 
+
+  "Version": "2012-10-17", 
+
+  "Statement": [ 
+
+{ 
+
+  "Action": "sts:AssumeRole", 
+
+  "Principal": { 
+
+  "Service": "ec2.amazonaws.com" 
+
+}, 
+
+  "Effect": "Allow", 
+
+  "Sid": "" 
+
+    } 
+
+  ] 
+
+} 
+
+EOF 
+
 } 
