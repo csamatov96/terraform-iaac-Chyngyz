d08cec7591c4365efb1f942be79c37a0988461a6
csamatov96 <csamatov96@gmail.com>
2020-01-04 13:39:02 -0600

    added tags

--- Environments/ec2.tf
+++ Environments/ec2.tf
@@ -2,5 +2,7 @@ resource "aws_instance" "INSTANCE" {
     ami = var.ami 
     instance_type = var.instance_type 
     key_name = var.key_name
-  
+    tags = {
+        Name = var.environment 
+    }
 }
