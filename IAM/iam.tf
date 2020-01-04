e690b16e2f46e79826e3af1820209da52d94196d
csamatov96 <csamatov96@gmail.com>
2020-01-04 11:19:19 -0600

    policy added

--- IAM/iam.tf
+++ IAM/iam.tf
@@ -1,31 +1,48 @@
 resource "aws_iam_user" "tim" { 
-
    name = "tim" 
-
    path = "/" 
-
 } 
 
 resource "aws_iam_group" "developers" { 
-
     name = "developers" 
-
     path = "/" 
+} 
+
+resource "aws_iam_group_membership" "team" {  #for terraform, specifying users to groups, terraform way of doing 
+  name = "developers-group-membership" 
+  users = [ 
+    aws_iam_user.tim.name 
+  ] 
+  group = aws_iam_group.developers.name # [, can be part of multiple groups] 
+} 
+
+resource "aws_iam_policy" "policy" { 
+  name = "test_policy3" 
+  path = "/" 
+  description = "My test policy" 
+  policy = <<EOF 
+{ 
+
+"Version": "2012-10-17", 
+
+"Statement": [ 
+
+{ 
+
+"Sid": "VisualEditor0", 
+
+"Effect": "Allow", 
+
+"Action": "ec2:*", 
+
+"Resource": "*" 
 
 } 
 
-resource "aws_iam_group_membership" "team" { 
+] 
 
-  name = "developers-group-membership" 
+} 
 
-  users = [ 
-
-    aws_iam_user.tim.name 
-
-  ] 
-
-  group = aws_iam_group.developers.name 
-
- 
+EOF 
 
 } 
