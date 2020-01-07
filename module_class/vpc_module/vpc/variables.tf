1878f9f77489137642cb638f8793dc19937de21c
csamatov96 <csamatov96@gmail.com>
2020-01-06 18:59:15 -0600

    vpc module added

---
+++ module_class/vpc_module/vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "name" {} 
+
+variable "cidr" {} 
+
+variable "public_subnet" {} 
+
+ 
+
+variable "enable_dns_hostnames" { 
+
+  default = true 
+
+} 
+
+variable "enable_dns_support" { 
+
+  default = true 
+
+} 
