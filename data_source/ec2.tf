566ac85ff04055def233fe7cd681883150594fce
csamatov96 <csamatov96@gmail.com>
2020-01-08 19:01:22 -0600

    changes

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -6,7 +6,7 @@ provider "aws" {
 #search for a Ubuntu latest with the owner
 data "aws_ami" "ubuntu" { #i can call it latest or whatever 
     filter { 
-        name   = "root-device-type" 
+        name = "root-device-type" 
         values = ["ebs"] #instance_store  
         } 
     most_recent = true 
