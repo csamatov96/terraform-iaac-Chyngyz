09483420bfc32affe734e385fab5b845ffad71bf
csamatov96 <csamatov96@gmail.com>
2020-01-13 16:51:39 -0600

    sent

--- old/environments/backend.tf
+++ old/environments/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "terrraform-us-chyngyz" #backend-iaac-chyngyz
     key    = "aws/ec2/ec2_state" #iam/iam_state
-    region = "us-east-1"
+    region = "us-east-1" #
   }
 }
