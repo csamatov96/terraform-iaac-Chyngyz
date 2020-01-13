fc81fb1f2410cb560b05080e09bc01f423223bc1
csamatov96 <csamatov96@gmail.com>
2020-01-13 16:18:51 -0600

    changes

--- old/module_class/vpc_module/vpc/vpc.tf
+++ old/module_class/vpc_module/vpc/vpc.tf
@@ -1,33 +1,20 @@
-resource "aws_vpc" "tfb" { 
-
+resource "aws_vpc" "vpc" { 
   cidr_block           = var.cidr 
-
   enable_dns_hostnames = var.enable_dns_hostnames 
-
   enable_dns_support   = var.enable_dns_support 
-
 } 
 
-resource "aws_internet_gateway" "tfb" { 
-
-  vpc_id = aws_vpc.tfb.id 
-
+resource "aws_internet_gateway" "igw" { 
+  vpc_id = aws_vpc.vpc.id 
 } 
 
 resource "aws_route" "internet_access" { 
-
-  route_table_id         = aws_vpc.tfb.main_route_table_id 
-
+  route_table_id         = aws_vpc.vpc.main_route_table_id 
   destination_cidr_block = "0.0.0.0/0" 
-
-  gateway_id             = aws_internet_gateway.tfb.id 
-
+  gateway_id             = aws_internet_gateway.igw.id 
 } 
 
 resource "aws_subnet" "public" { 
-
-  vpc_id     = aws_vpc.tfb.id 
-
+  vpc_id     = aws_vpc.vpc.id 
   cidr_block = var.public_subnet 
-
 } 
