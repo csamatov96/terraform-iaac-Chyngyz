b11cf72489ccb68dca956124061aacff48d714a7
root <root@ip-172-31-43-179.eu-west-1.compute.internal>
2020-01-14 02:16:35 +0000

    dynamodb added

--- app/backend.tf
+++ app/backend.tf
@@ -2,6 +2,8 @@ terraform {
 backend "s3" { 
 bucket = "wordpress-chyngyz-2019-dev" 
 key = "application/us-east-1/state/dev/infrastructure.tfstate" 
+dynamodb_table = "state_lock" #u can give any name u want 
 region = "us-east-1" 
+
   } 
 }
